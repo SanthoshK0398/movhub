9fab75bb2da872d287ef903c61c23eb6c12368e0
Santhosh Kumar K <santhoshkumark@Santhoshs-Mac-mini.local>
2025-07-05 00:05:57 +0530

    vercel deploy error fix

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/movhub/',
+  
   plugins: [react(),
     tailwindcss(),],
-  base: process.env.VITE_BASE_PATH || '/movhub'
+  base: process.env.VITE_BASE_PATH || "/movhub",
 })
